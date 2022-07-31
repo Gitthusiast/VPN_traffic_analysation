7ece36104b5b6d1cfa410adbdd82f60443b0e030
Gitthusiast <dorfmag@epost.bgu.ac.il>
2022-07-31 19:17:47 +0300

    added feature aggregation - std and mean on packet len and time_delta

--- dataEngineering.py
+++ dataEngineering.py
@@ -2,6 +2,7 @@
 import csv
 import os
 import pandas as pd
+import numpy as np
 
 browsing_type = {
     'Skype_chat': 'Chat',
@@ -32,99 +33,92 @@ browsing_type = {
     'MicrosoftTeams': 'Video Conferencing'
 }
 
+TIME_EPOCH = 0
+FRAME_LEN = 1
+SRC_PORT = 2
+
+IN_PACKET = 1
+OUT_PACKET = 0
 
 def data_preprocessing():
 
-    # filename = '.\\place1\\England\\Dropbox_VPN_1.5hr_England.tsv'
+    dirs = ['.\\place1\\England']  # , '.\\place2\\Japan']
+    for directory in dirs:
 
-        dirs = ['.\\place1\\England', '.\\place2\\Japan']
-        for directory in dirs:
+        # write all data into output labeled file
+        with open(f'.\\labeled_{directory[9:]}.tsv', 'wt', newline='') as out_file:
+            time = 0.0
+            prev_time = 0.0
+            tsv_writer = csv.writer(out_file, delimiter='\t')
 
-            # write all data into output labeled file
-            with open(f'.\\labeled_{directory[9:]}.tsv', 'wt', newline='') as out_file:
-                time = 0.0
-                prev_time = 0.0
-                tsv_writer = csv.writer(out_file, delimiter='\t')
+            tsv_writer.writerow(['frame.time_epoch', 'frame.len', 'tcp.srcport',
+                                 'Random website', 'Browsing', 'Country', 'in/out', 'time_delta'])
 
-                tsv_writer.writerow(['frame.time_epoch', 'frame.len', 'tcp.srcport',
-                                     'Random website', 'Browsing', 'Country', 'time_delta'])
+            for filename in os.listdir(directory):
+                with open(directory + '\\' + filename) as file:
+                    tsv_file = csv.reader(file, delimiter="\t")
+                    app_name = filename.split('_')
+                    # extract label from filename
+                    if not 'VPN' in app_name:
+                        label = '_'.join(app_name[:app_name.index('NONVPN')])
+                    else:
+                        label = '_'.join(app_name[:app_name.index('VPN')])
+                    print(label)
 
-                for filename in os.listdir(directory):
-                    with open(directory + '\\' + filename) as file:
-                        tsv_file = csv.reader(file, delimiter="\t")
-                        app_name = filename.split('_')
-                        # extract label from filename
-                        if not 'VPN' in app_name:
-                            label = '_'.join(app_name[:app_name.index('NONVPN')])
+                    for i, line in enumerate(tsv_file):
+                        if i == 0:  # skip heading
+                            continue
+                        prev_time = time
+                        time = float(line[0])
+                        in_out = OUT_PACKET if int(line[SRC_PORT]) >= 49151 else IN_PACKET
+                        # count delta frame.time between consecutive packets
+                        if i == 1:
+                            tsv_writer.writerow([line[TIME_EPOCH], line[FRAME_LEN], line[SRC_PORT], label,
+                                                 browsing_type[label], directory[9:], in_out, 0])
                         else:
-                            label = '_'.join(app_name[:app_name.index('VPN')])
-                        print(label)
-
-                        for i, line in enumerate(tsv_file):
-                            if i == 0:  # skip heading
-                                continue
-                            prev_time = time
-                            time = float(line[0])
-
-                            # count delta frame.time between consecutive packets
-                            if i == 1:
-                                tsv_writer.writerow([line[0], line[1], line[2], label, browsing_type[label],
-                                                     directory[9:], 0])
-                            else:
-                                tsv_writer.writerow([line[0], line[1], line[2], label, browsing_type[label],
-                                                     directory[9:], time-prev_time])
+                            tsv_writer.writerow([line[TIME_EPOCH], line[FRAME_LEN], line[SRC_PORT], label,
+                                                 browsing_type[label], directory[9:], in_out, time-prev_time])
 
 
-def data_preprocess(init_df):
+def feature_aggregation(init_df):
 
-    init_df.drop(columns=['ip.tos', 'tcp.options.mss_val', 'ip.opt.mtu'], inplace=True)
-    # init_df['tcp.options.mss_val'].fillna(method='ffill', inplace=True)
-    init_df.dropna(inplace=True)
+    last_row = int(init_df.shape[0]) + 1 if int(init_df.shape[0])//10 >0 else 0
+    stream_key = [int(i/10) for i in range(init_df.shape[0])]
+    init_df = init_df.assign(stream_key=stream_key)
 
-    """init_df['tcp.srcport'] = np.where(init_df['tcp.srcport'] > 1024, 1, 0)
-    init_df['tcp.dstport'] = np.where(init_df['tcp.dstport'] > 1024, 1, 0)"""
-
-    init_df['ip.dsfield'] = init_df['ip.dsfield'].apply(str).apply(int, base=16)
-    init_df['tcp.flags'] = init_df['tcp.flags'].apply(str).apply(int, base=16)
-    init_df['ip.flags'] = init_df['ip.flags'].apply(str).apply(int, base=16)
-
-    init_df.sort_values(['tcp.stream', 'frame.time_relative'], inplace=True, ignore_index=True)
-
-    init_df = count_delta_time_average_and_std_per_10_packets(init_df)
-    init_df = average_and_std_packet_len_per_10_packets(init_df)
-    init_df.dropna(inplace=True)
+    init_df = count_delta_time_average_and_std_per_10_packets(init_df, last_row)
+    # init_df = average_and_std_packet_len_per_10_packets(init_df)
+    # init_df.dropna(inplace=True)
 
     return init_df
 
 
-def count_delta_time_average_and_std_per_10_packets(init_df):
+def count_delta_time_average_and_std_per_10_packets(init_df, last_row):
 
-    init_df['stream_key'] = 0
-    stream_num = init_df['tcp.stream'][0]
-    counter, stream_index = 1, 1
+    """counter = 1
+    dt_vals = []
 
     for row in range(init_df.shape[0]):
 
-        if init_df['tcp.stream'][row] != stream_num:
-            stream_num = init_df['tcp.stream'][row]
-            counter, stream_index = 1, stream_index + 1
-
-        elif counter > 10:
-            counter, stream_index = 1, stream_index + 1
-
-        init_df.at[row, 'stream_key'] = stream_index
+        if counter > 10:
+            for i in range(10):
+                init_df.loc[init_df.index[row-i], 'delta_time_average'] = np.average(dt_vals)
+                init_df.loc[init_df.index[row-i], 'delta_time_std'] = np.std(dt_vals)
+            counter = 1
+            dt_vals = []
         counter += 1
+        dt_vals.append(float(init_df.loc[init_df.index[row], 'time_delta']))"""
 
     average_values_dict = {}
     std_values_dict = {}
 
-    for i_stream in sorted(init_df['stream_key'].unique()):
-        mean = init_df.loc[init_df['stream_key'] == i_stream]['tcp.time_delta'].mean()
-        std = init_df.loc[init_df['stream_key'] == i_stream]['tcp.time_delta'].std()
+    for i_stream in range(last_row):
+        mean = init_df.loc[init_df['stream_key'] == i_stream]['time_delta'].mean()
+        std = init_df.loc[init_df['stream_key'] == i_stream]['time_delta'].std()
         average_values_dict[i_stream] = mean
         std_values_dict[i_stream] = std
-    init_df['average_time_delta'] = init_df['stream_key'].apply(set_row_feature, args=(average_values_dict,))
-    init_df['std_time_delta'] = init_df['stream_key'].apply(set_row_feature, args=(std_values_dict,))
+    init_df['average_delta_time'] = init_df['stream_key'].apply(set_row_feature, args=(average_values_dict,))
+    init_df['std_delta_time'] = init_df['stream_key'].apply(set_row_feature, args=(std_values_dict,))
 
     return init_df
 
@@ -155,5 +149,7 @@ def set_row_feature(row_value, values_dict):
 if __name__ == '__main__':
 
     data_preprocessing()
-    # df = pd.read_csv('.\\labeled.tsv', sep='\t')
-
+    train_df = pd.read_csv('.\\labeled_England.tsv', sep='\t')
+    feature_aggregation(train_df)
+    train_df.to_csv('.\\final_labeled_England.tsv', sep='\t', index=False)
+    print('fin')
